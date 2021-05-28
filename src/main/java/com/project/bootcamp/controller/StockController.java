4ccbde46fb6ff1cc0512b392227a5dd439cd948c
Júnior Oliveira <juninsoader@gmail.com>
2021-05-28 11:55:16 -0300

    backend finished

--- src/main/java/com/project/bootcamp/controller/StockController.java
+++ src/main/java/com/project/bootcamp/controller/StockController.java
@@ -1,64 +1,50 @@
 package com.project.bootcamp.controller;
 
 import com.project.bootcamp.model.dto.StockDTO;
+import com.project.bootcamp.service.StockService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/stock")
 public class StockController {
 
+    @Autowired
+    private StockService service;
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<StockDTO> save(@Valid @RequestBody StockDTO dto){
-        return ResponseEntity.ok(dto);
+        return ResponseEntity.ok(service.save(dto));
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<StockDTO> update(@Valid @RequestBody StockDTO dto){
-        return ResponseEntity.ok(dto);
+        return ResponseEntity.ok(service.update(dto));
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<StockDTO>> findAll(){
-        List<StockDTO> list = new ArrayList<>();
-        StockDTO dto = new StockDTO();
-        dto.setId(1L);
-        dto.setName("teste");
-        dto.setPrice(100.0);
-        dto.setVariation(10.0);
-        dto.setDate(LocalDate.now());
-        list.add(dto);
-
-        return ResponseEntity.ok(list);
+        return ResponseEntity.ok(service.findAll());
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<StockDTO> findById(@PathVariable Long id){
-        List<StockDTO> list = new ArrayList<>();
-        StockDTO dto1 = new StockDTO();
-        dto1.setId(1L);
-        dto1.setName("teste1");
-        dto1.setPrice(100.0);
-        dto1.setVariation(10.0);
-        dto1.setDate(LocalDate.now());
+        return ResponseEntity.ok(service.findById(id));
+    }
 
-        StockDTO dto2 = new StockDTO();
-        dto2.setId(2L);
-        dto2.setName("teste2");
-        dto2.setPrice(200.0);
-        dto2.setVariation(5.0);
-        dto2.setDate(LocalDate.now());
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> delete(@PathVariable Long id) {
+        return ResponseEntity.ok(service.delete(id));
+    }
 
-        list.add(dto1);
-        list.add(dto2);
-
-        StockDTO selectedDto = list.stream().filter(x -> x.getId().compareTo(id) == 0).findFirst().get();
-        return ResponseEntity.ok(selectedDto);
+    @GetMapping(value = "/today", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<StockDTO>> findByToday() {
+        return ResponseEntity.ok(service.findByToday());
     }
 }
